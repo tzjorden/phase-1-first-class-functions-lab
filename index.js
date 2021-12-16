482d4988aee23138e954f481870e17f7ec650471
Tenzin Jorden <tenden96@gmail.com>
2021-12-16 09:45:00 -0800

    commit tree 6378fb1d7eef6b890514deb2758a77dcc3bfae40

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const returnFirstTwoDrivers=(drivers)=> {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    const length = drivers.length
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+const createFareMultiplier = (multiplier) => {
+    return(fare) => {
+        return fare* multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3)
+
+let cost = fareDoubler(50);
+
+cost = fareTripler(50)
+
+const selectDifferentDrivers = (drivers, fn) => {
+
+}
+
+selectDifferentDrivers([drivers], returnFirstTwoDrivers);
+selectDifferentDrivers([drivers], returnLastTwoDrivers);
